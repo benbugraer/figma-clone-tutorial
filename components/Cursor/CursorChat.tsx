39611397428f1f2b424c5790335ca05e798908de
benbugraer <bugraerdev@gmail.com>
2024-10-07 16:28:54 +0300

    Project setup & liveblocks integration & live cursor settings

---
+++ components/Cursor/CursorChat.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CursorChat = () => {
+  return <div>Cursor Chat</div>;
+};
+
+export default CursorChat;
---
+++ components/Live.tsx
@@ -0,0 +1,45 @@
+import { useMyPresence } from "@/liveblocks.config";
+import { useOthers } from "@liveblocks/react";
+import { useCallback } from "react";
+import LiveCursors from "./Cursor/LiveCursor";
+
+const Live = () => {
+  const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  return (
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center border-2 border-green-500"
+    >
+      <h1 className="text-xl text-white">Liveblocks Figma Clone</h1>
+      <LiveCursors others={others} />
+    </div>
+  );
+};
+
+export default Live;
