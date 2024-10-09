8296fa28cfa89c04dc9d6ccbb064f34af2151133
benbugraer <bugraerdev@gmail.com>
2024-10-09 17:57:39 +0300

    LiveCursorChat feature activated

--- components/Cursor/Cursor.tsx
+++ components/Cursor/Cursor.tsx
@@ -11,12 +11,20 @@ const Cursor = ({ color, x, y, message }: Props) => {
   return (
     <div
       className="pointer-events-none absolute top-0 left-0"
-      style={{ transform: `translateX(${x}px)) translateY(${y}px))` }}
+      style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
     >
       <CursorSVG color={color} />
 
-      {/* Message */}
-      <h1>{message}</h1>
+      {message && (
+        <div
+          className="absolute left-2 top-5 rounded-3xl px-4 py-2"
+          style={{ backgroundColor: color }}
+        >
+          <p className="text-white whitespace-nowrap text-sm leading-relaxed">
+            {message}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
--- components/Live.tsx
+++ components/Live.tsx
@@ -1,11 +1,18 @@
 import { useMyPresence } from "@/liveblocks.config";
 import { useOthers } from "@liveblocks/react";
-import { useCallback } from "react";
+import { useCallback, useState, useEffect } from "react";
 import LiveCursors from "./Cursor/LiveCursor";
+import { CursorMode } from "@/types/type";
+import CursorChat from "./Cursor/CursorChat";
 
 const Live = () => {
   const others = useOthers();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden,
+  });
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
@@ -16,9 +23,9 @@ const Live = () => {
     updateMyPresence({ cursor: { x, y } });
   }, []);
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
-
+    setCursorState({ mode: CursorMode.Hidden });
     updateMyPresence({ cursor: null, message: null });
   }, []);
 
@@ -29,14 +36,55 @@ const Live = () => {
     updateMyPresence({ cursor: { x, y } });
   }, []);
 
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === "*") {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: "",
+        });
+      } else if (e.key === "Escape") {
+        updateMyPresence({ message: "" });
+        setCursorState({
+          mode: CursorMode.Hidden,
+        });
+      }
+    };
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "*") {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [updateMyPresence]);
+
   return (
     <div
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
-      className="h-[100vh] w-full flex justify-center items-center text-center border-2 border-green-500"
+      className="h-[100vh] w-full flex justify-center items-center text-center"
     >
       <h1 className="text-xl text-white">Liveblocks Figma Clone</h1>
+
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
+
       <LiveCursors others={others} />
     </div>
   );
